45ca756621c69773fdbea000b2e707362eb92bfe
Adeel Zahid <47818499+muhammadadeelzahid@users.noreply.github.com>
2023-05-25 13:33:11 +0500

    Update gps_parser.h

--- gps_parser.h
+++ gps_parser.h
@@ -6,10 +6,16 @@
 #define MAX_PACKET_LENGTH 128
 
 typedef struct {
-    char t[11];
-    char latitude[12];
-    char longitude[13];
-    // Add more fields as per your requirements
+    char t[11]; // Time
+    char latitude[10]; // Latitude
+    char latitude_direction[2]; // Latitude direction (N/S)
+    char longitude[11]; // Longitude
+    char longitude_direction[2]; // Longitude direction (E/W)
+    char fix_quality[2]; // Fix quality
+    char num_satellites[3]; // Number of satellites
+    char hdop[5]; // Horizontal Dilution of Precision (HDOP)
+    char altitude[8]; // Altitude
+    char height_geoid[8]; // Height of Geoid above WGS84 ellipsoid
 } GPSData;
 
 bool validate_checksum(const char *packet);
