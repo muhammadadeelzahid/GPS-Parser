719424e014efc444c4c70928673e2b8788563e82
Adeel Zahid <47818499+muhammadadeelzahid@users.noreply.github.com>
2023-05-25 13:32:37 +0500

    Update gps_parser.c
    Added additional fields from the NMEA String

--- gps_parser.c
+++ gps_parser.c
@@ -21,6 +21,7 @@ bool validate_checksum(const char *packet) {
 
     return (checksum == calculated_checksum);
 }
+
 void parse_gps_data(const char *packet, GPSData *gpsData) {
     if (packet == NULL || gpsData == NULL) {
         return;
@@ -49,12 +50,67 @@ void parse_gps_data(const char *packet, GPSData *gpsData) {
     strncpy(gpsData->latitude, &packet[tokenStart], tokenEnd - tokenStart);
     gpsData->latitude[tokenEnd - tokenStart] = '\0';
 
+    // Copy the latitude direction data
+    tokenStart = tokenEnd + 1; // Start index of the latitude direction token
+    strncpy(gpsData->latitude_direction, &packet[tokenStart], 1);
+    gpsData->latitude_direction[1] = '\0';
+
     // Copy the longitude data
-    tokenStart = tokenEnd + 3; // Start index of the longitude token
+    tokenStart = tokenEnd + 4; // Start index of the longitude token
     tokenEnd = tokenStart;
     while (packet[tokenEnd] != ',' && packet[tokenEnd] != '\0') {
         tokenEnd++;
     }
     strncpy(gpsData->longitude, &packet[tokenStart], tokenEnd - tokenStart);
     gpsData->longitude[tokenEnd - tokenStart] = '\0';
+
+    // Copy the longitude direction data
+    tokenStart = tokenEnd + 1; // Start index of the longitude direction token
+    strncpy(gpsData->longitude_direction, &packet[tokenStart], 1);
+    gpsData->longitude_direction[1] = '\0';
+
+    // Copy the fix quality data
+    tokenStart = tokenEnd + 3; // Start index of the fix quality token
+    tokenEnd = tokenStart;
+    while (packet[tokenEnd] != ',' && packet[tokenEnd] != '\0') {
+        tokenEnd++;
+    }
+    strncpy(gpsData->fix_quality, &packet[tokenStart], tokenEnd - tokenStart);
+    gpsData->fix_quality[tokenEnd - tokenStart] = '\0';
+
+    // Copy the number of satellites data
+    tokenStart = tokenEnd + 1; // Start index of the number of satellites token
+    tokenEnd = tokenStart;
+    while (packet[tokenEnd] != ',' && packet[tokenEnd] != '\0') {
+        tokenEnd++;
+    }
+    strncpy(gpsData->num_satellites, &packet[tokenStart], tokenEnd - tokenStart);
+    gpsData->num_satellites[tokenEnd - tokenStart] = '\0';
+
+    // Copy the HDOP data
+    tokenStart = tokenEnd + 1; // Start index of the HDOP token
+    tokenEnd = tokenStart;
+    while (packet[tokenEnd] != ',' && packet[tokenEnd] != '\0') {
+        tokenEnd++;
+    }
+    strncpy(gpsData->hdop, &packet[tokenStart], tokenEnd - tokenStart);
+    gpsData->hdop[tokenEnd - tokenStart] = '\0';
+
+    // Copy the altitude data
+    tokenStart = tokenEnd + 1; // Start index of the altitude token
+    tokenEnd = tokenStart;
+    while (packet[tokenEnd] != ',' && packet[tokenEnd] != '\0') {
+        tokenEnd++;
+    }
+    strncpy(gpsData->altitude, &packet[tokenStart], tokenEnd - tokenStart);
+    gpsData->altitude[tokenEnd - tokenStart] = '\0';
+
+    // Copy the height of geoid data
+    tokenStart = tokenEnd + 3; // Start index of the height of geoid token
+    tokenEnd = tokenStart;
+    while (packet[tokenEnd] != ',' && packet[tokenEnd] != '\0') {
+        tokenEnd++;
+    }
+    strncpy(gpsData->height_geoid, &packet[tokenStart], tokenEnd - tokenStart);
+    gpsData->height_geoid[tokenEnd - tokenStart] = '\0';
 }
