d6d098ea602d9366991e4fca6d006857dc1753d5
Adeel Zahid <adeel.m.zahid@gmail.com>
2023-06-07 02:36:06 +0500

    Update gps_parser.c

--- gps_parser.c
+++ gps_parser.c
@@ -5,112 +5,112 @@
 bool validate_checksum(const char *packet) {
     int i = 1;
     char checksum = 0;
-
+    
+    //Calculating the checksum of recieved data uptil * character 
     while (packet[i] != '*' && packet[i] != '\0') {
         checksum = checksum ^ packet[i];
         i++;
     }
 
-    char extracted_checksum[3];
-    extracted_checksum[0] = packet[i + 1];
-    extracted_checksum[1] = packet[i + 2];
+    //Extracting the checksum from the string - last 3 characters
+    char extracted_checksum[3]; 
+    int length = strlen(packet);  
+    extracted_checksum[0] = packet[length-2];
+    extracted_checksum[1] = packet[length-1];
     extracted_checksum[2] = '\0';
 
+    //converts calculated checksum in to long integer
     char *endptr;
     char calculated_checksum = strtol(extracted_checksum, &endptr, 16);
+    
+    //Null packet  
+    if (packet == NULL) {
+        printf("NULL Found\n");
+        return 0;
+    }
 
+    //Invalid format of packet or empty string
+    if (strncmp(packet, "$GPGGA,", 7) != 0) {
+        // Not a GGA packet
+        printf("Invalid Packet\n");
+        return 0;
+    }
     return (checksum == calculated_checksum);
 }
 
 void parse_gps_data(const char *packet, GPSData *gpsData) {
-    if (packet == NULL || gpsData == NULL) {
-        return;
+
+    // Create a copy of the packet to avoid modifying the original string
+    char copy[MAX_PACKET_LENGTH];
+    strncpy(copy, packet, MAX_PACKET_LENGTH);
+
+    char *token;
+    // Extract individual parameter, this will return first token before first comma 
+    token = strtok(copy, ",");
+    int i = 0;
+    while (token != NULL && i < 14) {
+        switch (i) {
+            case 1:
+                // Copy the token to the `time` field of `gpsData`
+                strncpy(gpsData->time, token, sizeof(gpsData->time));
+                break;
+            case 2:
+                // Convert the token to a floating-point latitude value
+                gpsData->latitude = strtof(token, NULL);
+                break;
+            case 3:
+                // Store the first character of the token as the latitude direction
+                gpsData->latitude_dir = token[0];
+                break;
+            case 4:
+                // Convert the token to a floating-point longitude value
+                gpsData->longitude = strtof(token, NULL);
+                break;
+            case 5:
+                // Store the first character of the token as the longitude direction
+                gpsData->longitude_dir = token[0];
+                break;
+            case 6:
+                // Convert the token to an integer fix quality value
+                gpsData->fix_quality = atoi(token);
+                break;
+            case 7:
+                // Convert the token to an integer number of satellites value
+                gpsData->num_satellites = atoi(token);
+                break;
+            case 8:
+                // Convert the token to a floating-point HDOP (horizontal dilution of precision) value
+                gpsData->hdop = strtof(token, NULL);
+                break;
+            case 9:
+                // Convert the token to a floating-point altitude value
+                gpsData->altitude = strtof(token, NULL);
+                break;
+            case 10:
+                // Store the first character of the token as the altitude direction
+                gpsData->altitude_dir = token[0];
+                break;
+            case 11:
+                // Convert the token to a floating-point geoid height value
+                gpsData->geoid_height = strtof(token, NULL);
+                break;
+            case 12:
+                // Store the first character of the token as the geoid height direction
+                gpsData->geoid_height_dir = token[0];
+                break;
+            case 13:
+                // Convert the token to a floating-point time since DGPS (Differential GPS) update value
+                gpsData->time_since_dgps_update = strtof(token, NULL);
+                break;
+        }
+        // Get the next token by calling strtok() with NULL as the first argument
+        token = strtok(NULL, ",");
+        i++;
     }
 
-    if (strncmp(packet, "$GPGGA,", 7) != 0) {
-        // Not a GGA packet
-        return;
-    }
-
-    // Copy the time data
-    int tokenStart = 7; // Start index of the time token
-    int tokenEnd = tokenStart;
-    while (packet[tokenEnd] != ',' && packet[tokenEnd] != '\0') {
-        tokenEnd++;
-    }
-    strncpy(gpsData->t, &packet[tokenStart], tokenEnd - tokenStart);
-    gpsData->t[tokenEnd - tokenStart] = '\0';
-
-    // Copy the latitude data
-    tokenStart = tokenEnd + 1; // Start index of the latitude token
-    tokenEnd = tokenStart;
-    while (packet[tokenEnd] != ',' && packet[tokenEnd] != '\0') {
-        tokenEnd++;
-    }
-    strncpy(gpsData->latitude, &packet[tokenStart], tokenEnd - tokenStart);
-    gpsData->latitude[tokenEnd - tokenStart] = '\0';
-
-    // Copy the latitude direction data
-    tokenStart = tokenEnd + 1; // Start index of the latitude direction token
-    strncpy(gpsData->latitude_direction, &packet[tokenStart], 1);
-    gpsData->latitude_direction[1] = '\0';
-
-    // Copy the longitude data
-    tokenStart = tokenEnd + 4; // Start index of the longitude token
-    tokenEnd = tokenStart;
-    while (packet[tokenEnd] != ',' && packet[tokenEnd] != '\0') {
-        tokenEnd++;
-    }
-    strncpy(gpsData->longitude, &packet[tokenStart], tokenEnd - tokenStart);
-    gpsData->longitude[tokenEnd - tokenStart] = '\0';
-
-    // Copy the longitude direction data
-    tokenStart = tokenEnd + 1; // Start index of the longitude direction token
-    strncpy(gpsData->longitude_direction, &packet[tokenStart], 1);
-    gpsData->longitude_direction[1] = '\0';
-
-    // Copy the fix quality data
-    tokenStart = tokenEnd + 3; // Start index of the fix quality token
-    tokenEnd = tokenStart;
-    while (packet[tokenEnd] != ',' && packet[tokenEnd] != '\0') {
-        tokenEnd++;
-    }
-    strncpy(gpsData->fix_quality, &packet[tokenStart], tokenEnd - tokenStart);
-    gpsData->fix_quality[tokenEnd - tokenStart] = '\0';
-
-    // Copy the number of satellites data
-    tokenStart = tokenEnd + 1; // Start index of the number of satellites token
-    tokenEnd = tokenStart;
-    while (packet[tokenEnd] != ',' && packet[tokenEnd] != '\0') {
-        tokenEnd++;
-    }
-    strncpy(gpsData->num_satellites, &packet[tokenStart], tokenEnd - tokenStart);
-    gpsData->num_satellites[tokenEnd - tokenStart] = '\0';
-
-    // Copy the HDOP data
-    tokenStart = tokenEnd + 1; // Start index of the HDOP token
-    tokenEnd = tokenStart;
-    while (packet[tokenEnd] != ',' && packet[tokenEnd] != '\0') {
-        tokenEnd++;
-    }
-    strncpy(gpsData->hdop, &packet[tokenStart], tokenEnd - tokenStart);
-    gpsData->hdop[tokenEnd - tokenStart] = '\0';
-
-    // Copy the altitude data
-    tokenStart = tokenEnd + 1; // Start index of the altitude token
-    tokenEnd = tokenStart;
-    while (packet[tokenEnd] != ',' && packet[tokenEnd] != '\0') {
-        tokenEnd++;
-    }
-    strncpy(gpsData->altitude, &packet[tokenStart], tokenEnd - tokenStart);
-    gpsData->altitude[tokenEnd - tokenStart] = '\0';
-
-    // Copy the height of geoid data
-    tokenStart = tokenEnd + 3; // Start index of the height of geoid token
-    tokenEnd = tokenStart;
-    while (packet[tokenEnd] != ',' && packet[tokenEnd] != '\0') {
-        tokenEnd++;
-    }
-    strncpy(gpsData->height_geoid, &packet[tokenStart], tokenEnd - tokenStart);
-    gpsData->height_geoid[tokenEnd - tokenStart] = '\0';
+    // Extract the checksum from the last 2 characters of the packet
+    int size = strlen(packet);
+    gpsData->checksum[0] = packet[size-2];
+    gpsData->checksum[1] = packet[size-1];
+    gpsData->checksum[2] = '\0';
 }
